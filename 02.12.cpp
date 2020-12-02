2fdf59e2564d4d40ca09e22aed900fc7f1a2c393
kamykwp2 <74542115+kamykwp2@users.noreply.github.com>
2020-12-02 11:23:15 +0100

    Add files via upload

---
+++ 02.12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <random>
+int main()
+{
+    using namespace std;
+default_random_engine generator;
+uniform_int_distribution<int> distribution(1,6);
+int kostka = distribution(generator);
+return kostka;
+
+
+}
+
