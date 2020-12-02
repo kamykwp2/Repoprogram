28211b560520c2cf1c79fa8e92c8ec90fbb6e736
kamykwp2 <74542115+kamykwp2@users.noreply.github.com>
2020-12-02 12:13:21 +0100

    Update kart02.12.cpp

--- kart02.12.cpp
+++ kart02.12.cpp
@@ -4,10 +4,9 @@
 int main()
 {
     using namespace std;
-default_random_engine generator;
-uniform_int_distribution<int> distribution(1,6);
-int kostka = distribution(generator);
-
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(1,6);
+    int kostka = distribution(generator);
     for(int i =0, i<10, i++)
         cout << kostka;
     return 0;
